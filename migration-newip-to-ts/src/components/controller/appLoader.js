0a1e4ed9ca45aa3b82fe8208df011636d2005ec4
SiarheiHa <belposchta@gmail.com>
2022-06-24 17:59:19 +0300

    refactor: add ts and eslint

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'd89d2166fc4442d3a54187518bde8a56', // получите свой ключ https://newsapi.org/
         });
     }
 }
