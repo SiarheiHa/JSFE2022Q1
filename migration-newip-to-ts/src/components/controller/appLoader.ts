6010ed44ad483e72507a9c81845429896122dbc2
SiarheiHa <belposchta@gmail.com>
2022-07-03 03:24:35 +0300

    feat: add sourceLetterList

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -3,9 +3,13 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: 'd89d2166fc4442d3a54187518bde8a56', // получите свой ключ https://newsapi.org/
+            apiKey: '3e7f1bf9b1ec458fbb4e528dff7e81ae', // получите свой ключ https://newsapi.org/
         });
     }
 }
 
 export default AppLoader;
+
+// Api keys:
+// '3e7f1bf9b1ec458fbb4e528dff7e81ae'
+// 'd89d2166fc4442d3a54187518bde8a56'
--- migration-newip-to-ts/src/components/view/sources/sources.ts
+++ migration-newip-to-ts/src/components/view/sources/sources.ts
@@ -5,6 +5,8 @@ class Sources implements ISources {
     public draw(data: NewsSourceData[]): void {
         const fragment = document.createDocumentFragment();
         const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+        const sourcesLetterList = document.createElement('div');
+        const firstLetterSet = [...new Set(data.map((item) => item.name[0].toUpperCase()))];
 
         data.forEach((item: NewsSourceData) => {
             const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
@@ -15,8 +17,20 @@ class Sources implements ISources {
             fragment.append(sourceClone);
         });
 
+        firstLetterSet.forEach((letter) => {
+            const span = document.createElement('span');
+            span.innerHTML = letter;
+            span.dataset.letter = letter;
+            span.classList.add('letter');
+            sourcesLetterList.append(span);
+        });
+
+        sourcesLetterList.classList.add('letter-list');
+        (document.querySelector('main') as HTMLElement).prepend(sourcesLetterList);
         (document.querySelector('.sources') as HTMLElement).append(fragment);
     }
+    
+    public sort(event: Event) {}
 }
 
 export default Sources;
