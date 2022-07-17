f061d031a00911c2d8afe1a56df273bcf73a92d9
SiarheiHa <belposchta@gmail.com>
2022-07-18 01:20:29 +0300

    docs: add self-review for cross-check

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,5 +1,8 @@
 import './scss/style.scss';
 import { ShopApp } from './components/controller/app';
+import { selfReview } from './components/docs/selfReview';
 
 const shop = new ShopApp();
 shop.start();
+
+console.log(selfReview);
