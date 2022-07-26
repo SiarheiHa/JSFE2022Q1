bb42173b8f5eb731db59a54041a7e09a0d50210e
SiarheiHa <belposchta@gmail.com>
2022-07-26 12:40:47 +0300

    init: start task

---
+++ async-race/webpack.config.js
@@ -0,0 +1,75 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
+
+const devServer = (isDev) => (!isDev
+  ? {}
+  : {
+    devServer: {
+      open: true,
+      hot: true,
+      port: 8080,
+      static: {
+        directory: path.join(__dirname),
+      },
+    },
+  });
+
+module.exports = ({ development }) => ({
+  mode: development ? 'development' : 'production',
+  devtool: development ? 'inline-source-map' : false,
+  entry: './src/index',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'images/[hash][ext][query]',
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.(?:ico|png|svg|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    // new CopyPlugin({
+    //     patterns: [
+    //         {
+    //             from: path.resolve(__dirname, 'src', 'assets', 'favorite-svgrepo-com.svg'),
+    //             to: path.resolve(__dirname, 'dist', 'images', 'favorite-svgrepo-com.svg'),
+    //         },
+    //     ],
+    // }),
+  ],
+  ...devServer(development),
+});
