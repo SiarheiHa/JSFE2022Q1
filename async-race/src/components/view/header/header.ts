9da7d16c1934559142f9f2234947ca99104eb035
SiarheiHa <belposchta@gmail.com>
2022-07-29 13:56:11 +0300

    feat: add switch-page buttons

--- async-race/src/components/view/header/header.ts
+++ async-race/src/components/view/header/header.ts
@@ -1,16 +1,14 @@
 import createNode from '../../utils/createNode';
+import SwitchField from '../switchField/switchfield';
 
 export default class Header {
+  switchField: SwitchField = new SwitchField('header-radio', ['garage', 'winners'], ['to garage', 'to winners']);
+
   drawHeader() {
     console.log('drawHeader');
     const header = createNode({ tag: 'header', classes: ['header'] });
     const wrapper = createNode({ tag: 'div', classes: ['header-wrapper', 'wrapper'] });
-    const buttons = ['to garage', 'to winners'].map((value) => {
-      const button = createNode({ tag: 'button', classes: ['button', 'header__button'], inner: value }) as HTMLButtonElement;
-      button.value = value.slice(3);
-      return button;
-    });
-    wrapper.append(...buttons);
+    wrapper.append(this.switchField.createSwitcField());
     header.append(wrapper);
 
     document.body.prepend(header);
---
+++ async-race/src/components/view/switchField/switchfield.ts
@@ -0,0 +1,39 @@
+import createNode from '../../utils/createNode';
+
+export default class SwitchField {
+  textes: string[];
+
+  name: string;
+
+  values: string[];
+
+  constructor(name: string, values: string[], textes: string[]) {
+    this.name = name;
+    this.values = values;
+    this.textes = textes;
+  }
+
+  createSwitcField() {
+    const div = createNode({ tag: 'div', classes: ['switch-field'] });
+    const inputs = ['switch_left', 'switch_right']
+      .map((id: string, index: number) => createNode({
+        tag: 'input',
+        atributesAdnValues: [
+          ['type', 'radio'],
+          ['id', id],
+          ['name', this.name],
+          ['value', this.values[index]],
+        ],
+      }));
+    (inputs[0] as HTMLInputElement).checked = true;
+    const labels = inputs.map((input: HTMLElement, index: number) => createNode({
+      tag: 'label',
+      atributesAdnValues: [
+        ['for', input.id],
+      ],
+      inner: this.textes[index],
+    }));
+    inputs.forEach((input, index) => div.append(input, labels[index]));
+    return div;
+  }
+}
